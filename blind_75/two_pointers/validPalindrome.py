b172e7a963adc9213d6f6ccea8efc586776d85eb
Daniel <danielrwalicki@gmail.com>
2022-12-20 19:47:07 -0800

    valid palindrome

---
+++ blind_75/two_pointers/validPalindrome.py
@@ -0,0 +1,32 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+# and removing all non-alphanumeric characters, it reads the same forward and backward.
+# Alphanumeric characters include letters and numbers.
+s = "A man, a plan, a canal: Panama"
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                print(f"{s} is NOT a palindrome")
+                return False
+            l, r = l + 1, r - 1
+        print(f"{s} IS a palindrome")
+        return True
+
+    def alphaNum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
+
+
+test_solution = Solution()
+test_solution.isPalindrome(s)
